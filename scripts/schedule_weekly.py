27de40068c184e707021ed8c10d530d68a945fc3
contradiction29 <sora32127@gmail.com>
2025-11-11 20:48:52 +0900

    feat: set UV

--- scripts/schedule_weekly.py
+++ scripts/schedule_weekly.py
@@ -6,7 +6,6 @@ from app.main import send_poll_message, weekday_options_from_env, get_env
 async def main():
     token = get_env("DISCORD_BOT_TOKEN", required=True)
     channel_id = get_env("DISCORD_CHANNEL_ID", required=True)
-
     message = os.getenv("WEEKLY_MESSAGE")
     options = weekday_options_from_env(None)
     try:
